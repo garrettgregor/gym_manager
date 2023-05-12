3538020dd1ba4ee42bfc4f03dc8eae2d39dc0daa
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-11 19:51:31 -0600

    Test: order gyms index by created_at

--- spec/features/gyms/index_spec.rb
+++ spec/features/gyms/index_spec.rb
@@ -7,30 +7,27 @@ RSpec.describe "gyms index page", type: :feature do
   # As a visitor
   # When I visit '/gyms'
   # Then I see the name of each gym record in the system
-  before :each do
-    @gym1 = Gym.create!(name: "Movement",
+  it "can show all of the gyms" do
+    gym1 = Gym.create!(name: "Movement",
                       member_cost:  92,
                       guest_cost:   25,
                       in_colorado:  true)
-    @gym2 = Gym.create!(name: "The Spot",
+    gym2 = Gym.create!(name: "The Spot",
                       member_cost:  75,
                       guest_cost:   20,
                       in_colorado:  true)
-  end
-
-  it "can show all of the gyms" do
     visit "/gyms"
     # save_and_open_page
 
-    expect(page).to have_content("Gym Name: #{@gym1.name}")
-    expect(page).to have_content("Member Cost Per Month: $#{@gym1.member_cost}")
-    expect(page).to have_content("Day Rate for Guests: $#{@gym1.guest_cost}")
-    expect(page).to have_content("In Colorado: #{@gym1.in_colorado}")
+    expect(page).to have_content("Gym Name: #{gym1.name}")
+    expect(page).to have_content("Member Cost Per Month: $#{gym1.member_cost}")
+    expect(page).to have_content("Day Rate for Guests: $#{gym1.guest_cost}")
+    expect(page).to have_content("In Colorado: #{gym1.in_colorado}")
 
-    expect(page).to have_content("Gym Name: #{@gym2.name}")
-    expect(page).to have_content("Member Cost Per Month: $#{@gym2.member_cost}")
-    expect(page).to have_content("Day Rate for Guests: $#{@gym2.guest_cost}")
-    expect(page).to have_content("In Colorado: #{@gym2.in_colorado}")
+    expect(page).to have_content("Gym Name: #{gym2.name}")
+    expect(page).to have_content("Member Cost Per Month: $#{gym2.member_cost}")
+    expect(page).to have_content("Day Rate for Guests: $#{gym2.guest_cost}")
+    expect(page).to have_content("In Colorado: #{gym2.in_colorado}")
   end
 
   # User Story 6, Parent Index sorted by Most Recently Created
@@ -39,4 +36,21 @@ RSpec.describe "gyms index page", type: :feature do
   # When I visit the parent index,
   # I see that records are ordered by most recently created first
   # And next to each of the records I see when it was created
+  it "can show all of the gyms" do
+    gym2 = Gym.create!(name: "Touchstone",
+                        member_cost:  100,
+                        guest_cost:   30,
+                        in_colorado:  false)
+    gym1 = Gym.create!(name: "Movement",
+                        member_cost:  92,
+                        guest_cost:   25,
+                        in_colorado:  true)
+    visit "/gyms"
+    save_and_open_page
+
+    expect("Gym Name: #{gym2.name}").to appear_before("Gym Name: #{gym1.name}")
+    expect("Member Cost Per Month: $#{gym2.member_cost}").to appear_before("Member Cost Per Month: $#{gym1.member_cost}")
+    expect("Day Rate for Guests: $#{gym2.guest_cost}").to appear_before("Day Rate for Guests: $#{gym1.guest_cost}")
+    expect("In Colorado: #{gym2.in_colorado}").to appear_before("In Colorado: #{gym1.in_colorado}")
+  end
 end
