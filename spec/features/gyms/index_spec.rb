67a49eafef2fa3515b09077f500997bd9c9d3cfd
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-13 16:43:47 -0600

    Test: gym index order

--- spec/features/gyms/index_spec.rb
+++ spec/features/gyms/index_spec.rb
@@ -9,25 +9,21 @@ RSpec.describe "gyms index page", type: :feature do
   # Then I see the name of each gym record in the system
   it "can show all of the gyms" do
     gym1 = Gym.create!(name: "Movement",
-                      member_cost:  92,
-                      guest_cost:   25,
-                      in_colorado:  true)
+      member_cost: 92,
+      guest_cost: 25,
+      in_colorado: true)
     gym2 = Gym.create!(name: "The Spot",
-                      member_cost:  75,
-                      guest_cost:   20,
-                      in_colorado:  true)
+      member_cost: 75,
+      guest_cost: 20,
+      in_colorado: true)
     visit "/gyms"
     # save_and_open_page
 
     expect(page).to have_content("Gym Name: #{gym1.name}")
-    expect(page).to have_content("Member Cost Per Month: $#{gym1.member_cost}")
-    expect(page).to have_content("Day Rate for Guests: $#{gym1.guest_cost}")
-    expect(page).to have_content("In Colorado: #{gym1.in_colorado}")
+    expect(page).to have_content("Created At: #{gym1.created_at}")
 
     expect(page).to have_content("Gym Name: #{gym2.name}")
-    expect(page).to have_content("Member Cost Per Month: $#{gym2.member_cost}")
-    expect(page).to have_content("Day Rate for Guests: $#{gym2.guest_cost}")
-    expect(page).to have_content("In Colorado: #{gym2.in_colorado}")
+    expect(page).to have_content("Created At: #{gym2.created_at}")
   end
 
   # User Story 6, Parent Index sorted by Most Recently Created
@@ -36,21 +32,20 @@ RSpec.describe "gyms index page", type: :feature do
   # When I visit the parent index,
   # I see that records are ordered by most recently created first
   # And next to each of the records I see when it was created
-  it "can show all of the gyms" do
+  it "can show all of the gyms in the order they were created at" do
+    gym1 = Gym.create!(name: "Movement",
+                        member_cost:  92,
+                        guest_cost:   25,
+                        in_colorado:  true,
+                        created_at: Date.today - 1)
     gym2 = Gym.create!(name: "Touchstone",
                         member_cost:  100,
                         guest_cost:   30,
                         in_colorado:  false)
-    gym1 = Gym.create!(name: "Movement",
-                        member_cost:  92,
-                        guest_cost:   25,
-                        in_colorado:  true)
     visit "/gyms"
-    save_and_open_page
+    # save_and_open_page
 
     expect("Gym Name: #{gym2.name}").to appear_before("Gym Name: #{gym1.name}")
-    expect("Member Cost Per Month: $#{gym2.member_cost}").to appear_before("Member Cost Per Month: $#{gym1.member_cost}")
-    expect("Day Rate for Guests: $#{gym2.guest_cost}").to appear_before("Day Rate for Guests: $#{gym1.guest_cost}")
-    expect("In Colorado: #{gym2.in_colorado}").to appear_before("In Colorado: #{gym1.in_colorado}")
+    expect("Created At: #{gym2.created_at}").to appear_before("Created At: #{gym1.created_at}")
   end
 end
