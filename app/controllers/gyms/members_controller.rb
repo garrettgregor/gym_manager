62c0aef1528144e71b3e3fe35c01b5e996fe1c92
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-16 15:56:30 -0600

    Feature: create new gym member

--- app/controllers/gyms/members_controller.rb
+++ app/controllers/gyms/members_controller.rb
@@ -11,4 +11,23 @@ class Gyms::MembersController < ApplicationController
     @gym = Gym.find(params[:id])
     @members = @gym.members
   end
+
+  def new
+    @gym = Gym.find(params[:id])
+  end
+
+  def create
+    @gym = Gym.find(params[:id])
+    @member = @gym.members.create!(member_params)
+    redirect_to "/gyms/#{@gym.id}/members"
+  end
+
+  private
+  def member_params
+    params.permit(
+      :name,
+      :money_spent,
+      :co_resident
+    )
+  end
 end
