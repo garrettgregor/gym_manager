51fdb98e761fea30c09b14ad6ee11de35033f716
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-15 18:05:23 -0600

    Feature: create gym

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -8,6 +8,22 @@ class GymsController < ApplicationController
   end
 
   def new
-    
+
+  end
+
+  def create
+    gym = Gym.create(gym_params)
+    redirect_to "/gyms"
+  end
+
+  private
+
+  def gym_params
+    params.permit(
+      :name,
+      :member_cost,
+      :guest_cost,
+      :in_colorado
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/gyms", to: "gyms#index"
   get "/gyms/new", to: "gyms#new"
+  post "/gyms", to: "gyms#create"
   get "/gyms/:id", to: "gyms#show"
   get "/gyms/:id/members", to: "gyms/members#index"
   get "/members", to: "members#index"
