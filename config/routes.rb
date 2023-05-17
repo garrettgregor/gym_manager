12ae7f823f37a7fc3061b389125054617d86f116
Garrett Gregor <garrett.gregor@gmail.com>
2023-05-17 12:40:41 -0600

    Add welcome page and fix seeds

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/", to: "gyms#index"
   get "/gyms", to: "gyms#index"
+
   get "/gyms/new", to: "gyms#new"
   post "/gyms", to: "gyms#create"
+
   get "/gyms/:id", to: "gyms#show"
   get "/gyms/:id/edit", to: "gyms#edit"
   patch "/gyms/:id", to: "gyms#update"
+
   get "/gyms/:id/members", to: "gyms/members#index"
+
   get "/members", to: "members#index"
   get "/members/:id", to: "members#show"
+
   get "/gyms/:id/members/new", to: "gyms/members#new"
   post "/gyms/:id/members", to: "gyms/members#create"
+
   get "/members/:id/edit", to: "members#edit"
   patch "/members/:id", to: "members#update"
 end
